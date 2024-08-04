8e0b6056a4931d99d715b670b93c60ca57000400
cowboyHat123111 <cowboy.hat1@protonmail.com>
2024-08-04 17:25:52 +0300

    Add unit tests for user module

---
+++ dynammatrix.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+using namespace std;
+
+
+int main()
+{
+	srand(time(NULL));
+	int n;
+	cout << "Input number of rows and columns (square matrix)\n";
+	cin >> n;
+	int **matrix = new int*[n];
+
+	for (int i = 0;i < n;i++)
+	{
+		matrix[i] = new int[n];
+	}
+
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < n;j++)
+		{
+			matrix[i][j] = rand() % 10;
+			cout <<setw(3)<< matrix[i][j];
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < n;i++) 
+	{
+		delete[] matrix[i];
+	}
+
+	delete[] matrix;
+}
